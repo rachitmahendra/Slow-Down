b6d0b742c4fb2bd28fb011979de04a9c1cedf664
rachitmahendra <ghostlabs.2015@gmail.com>
2016-08-14 06:42:16 +0530

    Prepare Drink list

--- app/src/main/java/com/ghostlabs/slowdown/DrinksActivity.java
+++ app/src/main/java/com/ghostlabs/slowdown/DrinksActivity.java
@@ -1,6 +1,5 @@
 package com.ghostlabs.slowdown;
 
-import android.graphics.Movie;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -37,51 +36,11 @@ public class DrinksActivity extends MainActivity {
         Drink drink = new Drink("Beer", 0.07);
         drinksList.add(drink);
 
-        drink = new Drink("Mad Max: Fury Road", 0.07);
+        drink = new Drink("Liquor", 0.07);
         drinksList.add(drink);
 
-        movie = new Movie("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
-        movieList.add(movie);
 
-        movie = new Movie("Shaun the Sheep", "Animation", "2015");
-        movieList.add(movie);
-
-        movie = new Movie("The Martian", "Science Fiction & Fantasy", "2015");
-        movieList.add(movie);
-
-        movie = new Movie("Mission: Impossible Rogue Nation", "Action", "2015");
-        movieList.add(movie);
-
-        movie = new Movie("Up", "Animation", "2009");
-        movieList.add(movie);
-
-        movie = new Movie("Star Trek", "Science Fiction", "2009");
-        movieList.add(movie);
-
-        movie = new Movie("The LEGO Movie", "Animation", "2014");
-        movieList.add(movie);
-
-        movie = new Movie("Iron Man", "Action & Adventure", "2008");
-        movieList.add(movie);
-
-        movie = new Movie("Aliens", "Science Fiction", "1986");
-        movieList.add(movie);
-
-        movie = new Movie("Chicken Run", "Animation", "2000");
-        movieList.add(movie);
-
-        movie = new Movie("Back to the Future", "Science Fiction", "1985");
-        movieList.add(movie);
-
-        movie = new Movie("Raiders of the Lost Ark", "Action & Adventure", "1981");
-        movieList.add(movie);
-
-        movie = new Movie("Goldfinger", "Action & Adventure", "1965");
-        movieList.add(movie);
-
-        movie = new Movie("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
-        movieList.add(movie);
 
         mAdapter.notifyDataSetChanged();
-    }*/
+    }
 }
